5c827ccefec701b9ed67404f8b292d69febf15bf
heindesign <hein@yandex.com>
2018-11-22 19:51:45 +0500

    upload

---
+++ app/js/main.js
@@ -0,0 +1,30 @@
+var	cardList = document.querySelector('.catalog__list'),
+	totalPrice = 0,
+	count = 0;
+
+cardList.onclick = function(event) {
+	var target = event.target;
+	if (target.classList.contains('clothes__btn')) {	
+		var parentEl = target.parentNode;
+			price = parentEl.querySelector('.clothes__price'),
+			priceAttr = parseInt(price.getAttribute('data-price')),
+			basket = document.querySelector('.items-cost');
+
+		parentEl.classList.toggle('active');
+
+		if (parentEl.classList.contains('active')) {
+			totalPrice += priceAttr;
+			count += 1;
+		} else {
+			totalPrice -= priceAttr;
+			count -= 1;
+		}
+	}
+	if (totalPrice == 0) {
+		basket.textContent = 'Корзина';
+	} else {
+		basket.textContent = totalPrice + ' Р';
+	}
+	var itemsAmount = document.querySelector('.items-amount');
+	itemsAmount.textContent = count;
+}
